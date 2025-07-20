cef1a74d223b026c16b5c4794a511faa9970fe53
vivek-pareek2006 <vivek.pareek2006@gmail.com>
2025-07-20 11:52:52 +0530

    Init Java-Spring Application

---
+++ src/main/java/com/hackathon/healsync/HealsyncApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.healsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/healsync/HealsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.healsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
